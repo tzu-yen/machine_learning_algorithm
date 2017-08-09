ecf80e598057bba9d01a62d35732932040589084
tzuyen <tyen.wang@gmail.com>
2017-08-09 18:28:42 -0400

    add cloud and spiral function

--- deep_learning2/util.py
+++ deep_learning2/util.py
@@ -29,6 +29,52 @@ def get_normalized_data():
 	X = (X - mu) / std
 	return X, Y
 
+def get_clouds():
+    Nclass = 500
+    D = 2
+
+    X1 = np.random.randn(Nclass, D) + np.array([0, -2])
+    X2 = np.random.randn(Nclass, D) + np.array([2, 2])
+    X3 = np.random.randn(Nclass, D) + np.array([-2, 2])
+    X = np.vstack([X1, X2, X3])
+
+    Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
+    return X, Y
+
+def get_spiral():
+    # Idea: radius -> low...high
+    #           (don't start at 0, otherwise points will be "mushed" at origin)
+    #       angle = low...high proportional to radius
+    #               [0, 2pi/6, 4pi/6, ..., 10pi/6] --> [pi/2, pi/3 + pi/2, ..., ]
+    # x = rcos(theta), y = rsin(theta) as usual
+
+    radius = np.linspace(1, 10, 100)
+    thetas = np.empty((6, 100))
+    for i in range(6):
+        start_angle = np.pi*i / 3.0
+        end_angle = start_angle + np.pi / 2
+        points = np.linspace(start_angle, end_angle, 100)
+        thetas[i] = points
+
+    # convert into cartesian coordinates
+    x1 = np.empty((6, 100))
+    x2 = np.empty((6, 100))
+    for i in range(6):
+        x1[i] = radius * np.cos(thetas[i])
+        x2[i] = radius * np.sin(thetas[i])
+
+    # inputs
+    X = np.empty((600, 2))
+    X[:,0] = x1.flatten()
+    X[:,1] = x2.flatten()
+
+    # add noise
+    X += np.random.randn(600, 2)*0.5
+
+    # targets
+    Y = np.array([0]*100 + [1]*100 + [0]*100 + [1]*100 + [0]*100 + [1]*100)
+    return X, Y
+
 def plot_cumulative_variance(pca):
 	P = []
 	for p in pca.explained_variance_ratio:
